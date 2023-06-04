1f6ddf68764897180a0326e4f6c07edef6bc12a7
김성우 <126859882+seabear6400@users.noreply.github.com>
2023-06-04 14:32:15 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,10 +1,10 @@
 student=[]
-n_student=[]
+absent=[]
 for i in range(28):
-    s=int(input())
-    student.append(s)
+    Attend=int(input())
+    student.append(Attend)
 for i in range(1,31):
     if i not in student:
-        n_student.append(i)
-print(min(n_student))
-print(max(n_student))
+        absent.append(i)
+print(min(absent))
+print(max(absent))
